cff69d4157c932beef64a477d5599a086badf44e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-11-26 22:04:24 +0000

    Import all the scenes and add the to the Game class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import 'phaser';
+import Phaser from 'phaser';
 import config from './Config/config';
 import GameScene from './Scenes/game-scene';
+import BootScene from './Scenes/boot-scene';
+import PreloaderScene from './Scenes/preloader-scene';
+import TitleScene from './Scenes/title-scene';
+import OptionsScene from './Scenes/options-scene';
+import CreditsScene from './Scenes/credits-scene';
+
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Option', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
     this.scene.start('Game');
   }
